f4b65063e9a8fc7a082ae81c56fcf1daf3247c21
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-18 14:33:00 +0530

    Forge: AI Update: src/main/java/com/tcenema/magicwands/MagicWands.java

---
+++ src/main/java/com/tcenema/magicwands/MagicWands.java
@@ -0,0 +1,33 @@
+package com.tcenema.magicwands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MagicWands extends JavaPlugin {
+    private static MagicWands instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new WandListener(this), this);
+        registerRecipes();
+        getLogger().info("§b[MagicWands] Module Online. Forging complete.");
+    }
+
+    private void registerRecipes() {
+        for (WandType type : WandType.values()) {
+            ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, type.name().toLowerCase() + "_recipe"), type.getItem(this));
+            recipe.shape("  D", " S ", "S  ");
+            recipe.setIngredient('S', Material.STICK);
+            recipe.setIngredient('D', type.getRecipeCore());
+            Bukkit.addRecipe(recipe);
+        }
+    }
+
+    public static MagicWands getInstance() { return instance; }
+}
